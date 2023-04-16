f05c74ec87fa0d2dab4e2189e7a2ef093b3b7bfd
Abrar Ahmed Mohammed <abrarahmed647@gmail.com>
2023-04-16 12:02:23 -0400

    Abrar- Added CA cert file

--- search.py
+++ search.py
@@ -11,7 +11,7 @@ db_host = "ipfs-server.mysql.database.azure.com"
 db_user = "mrprcsuoxp"
 db_pass = "pass@cs6675db"
 db_name = "ipfs-db"
-conn = mysql.connector.connect(host=db_host, user=db_user, password=db_pass, database=db_name)
+conn = mysql.connector.connect(user=db_user, password=db_pass, host=db_host, database=db_name, ssl_ca="DigiCertGlobalRootCA.crt.pem", ssl_disabled=False)
 
 # Get the search query from the form
 form = cgi.FieldStorage()
